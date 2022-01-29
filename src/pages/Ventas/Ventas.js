2693e867a47d526cf6bdf60e95e865648054f655
Cesar Val Leiva <cesarvalleiva@gmail.com>
2022-01-29 20:35:28 -0300

    cambio en Ventas del import React

--- src/pages/Ventas/Ventas.js
+++ src/pages/Ventas/Ventas.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useContext } from 'react';
+import React, { useContext } from 'react';
 import { Context } from '../../store/appContext';
 import './Ventas.css'
 
